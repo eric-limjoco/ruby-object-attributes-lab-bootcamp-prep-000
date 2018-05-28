18012f1a1994649528e8dbc975f32788785973e8
ericlimjoco@gmail.com Limjoco <ericlimjoco@gmail.com>
2018-05-28 13:06:25 +0000

    automatically backed up by learn

---
+++ lib/person.rb
@@ -0,0 +1,23 @@
+class Person
+  
+  def initialize(name, job)
+    @name = name
+    @job = job
+  end
+  
+  def name
+    name
+  end
+  
+  def name=(n)
+    @name = n
+  end
+  
+  def job
+    @job
+  end
+  
+  def breed=(b)
+    @breed=b
+  end
+end
